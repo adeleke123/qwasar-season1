e12a989ae060fbbcd1aeba20e7fe31e78b9dcd24
Bakare Muideen Adeleke <bakaremuideenadeleke@yahoo.com>
2023-02-13 22:10:25 +0100

    Quest completed

---
+++ Quest01/06-my_first_if_else.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+  int nbr = 10;
+
+  if (10 > 20) {
+    printf("nbr is greater than 20\n");
+  }
+  else {
+    printf("nbr is less than 20\n");
+  }
+  return 0;
+}
