c831e26c4310e76082c8399d8bc9e252c24500b3
Bakare Muideen Adeleke <bakaremuideenadeleke@yahoo.com>
2023-02-13 22:08:47 +0100

    Quest completed

---
+++ Quest01/05-my_first_incrementation.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main() {
+  int my_index = 0;
+
+  // replace this comment with an increment
+  my_index +=1;
+  printf("%d\n", my_index);
+  my_index -=1;
+  my_index -=1;
+  // replace this comment with an decrement
+  // replace this comment with an decrement
+  printf("%d\n", my_index);
+  my_index +=1;
+  my_index +=1;
+  my_index +=1;
+  // replace this comment with an increment
+  // replace this comment with an increment
+  // replace this comment with an increment
+  printf("%d\n", my_index);
+  return 0;
+}
