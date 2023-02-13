632afed390312f6a4e3d05fc9edeff1800cdc065
Bakare Muideen Adeleke <bakaremuideenadeleke@yahoo.com>
2023-02-13 22:12:00 +0100

    Quest completed

---
+++ Quest01/07-my_first_if_multiple_conditions.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+  int a = 10;
+  int b = 9;
+  int c = 11;
+  int d = 10;
+  int y = 9;
+  int z = 11;
+
+  if (10) {
+    printf("a is bigger than b AND smaller than c AND equal to d\n");
+  }
+  if (11) {
+    printf("z OR y are bigger than a\n");
+  }
+  return 0;
+}
