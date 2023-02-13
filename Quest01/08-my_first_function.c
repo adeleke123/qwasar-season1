923564a1f7cc9840a013cb990c41568cf06075a6
Bakare Muideen Adeleke <bakaremuideenadeleke@yahoo.com>
2023-02-13 22:13:26 +0100

    Quest completed

---
+++ Quest01/08-my_first_function.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+// Following XXXXXX will be a function that will print using printf("my_first_function\n");
+void my_first_function()
+{
+   printf("my_first_function\n"); 
+}
+int main() {
+  my_first_function();
+
+  return 0;
+}
