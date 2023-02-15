89e27be63e9ae9c13542dfee5d401a11a9641225
Bakare Muideen Adeleke <bakaremuideenadeleke@yahoo.com>
2023-02-15 22:55:22 +0100

    Quest completed

---
+++ Quest01/14-my_isalpha.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * _isalpha - Check for lowercase ASCII characters
+ * @c: This is an ASCII character
+ * Return: 0 if not an alphabet, 1 if so
+ */
+int my_isalpha(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
