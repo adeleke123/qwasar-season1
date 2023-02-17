95ff035b9324a923ef6647a5b65a513b3a525d01
Bakare Muideen Adeleke <bakaremuideenadeleke@yahoo.com>
2023-02-17 21:59:43 +0100

    Quest completed

---
+++ Quest01/16-my_islower.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * _islower - Check for lowercase ASCII characters
+ * @c: This is an ASCII character
+ *
+ * Return: 0 if not lowercase, 1 if lowercase
+ */
+int my_islower(int c)
+{
+	if ((c >= 97) && (c <= 122))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
