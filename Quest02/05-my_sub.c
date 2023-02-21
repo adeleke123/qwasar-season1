d4a30e2643a9316f424129a41794a15a4900519e
Bakare Muideen Adeleke <bakaremuideenadeleke@yahoo.com>
2023-02-21 21:38:20 +0100

    Quest completed

---
+++ Quest02/05-my_sub.c
@@ -0,0 +1,15 @@
+/*
+**
+** QWASAR.IO -- my_sub
+**
+** @param {int} param_1
+** @param {int} param_2
+**
+** @return {int}
+**
+*/
+
+int my_sub(int param_1, int param_2)
+{
+    return(param_1 - param_2);
+}
