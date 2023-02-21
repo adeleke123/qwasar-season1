f14af9010b1f11ec46f6d84e89fce0b1a365fbc6
Bakare Muideen Adeleke <bakaremuideenadeleke@yahoo.com>
2023-02-21 21:36:51 +0100

    Quest completed

---
+++ Quest02/04-my_add.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/*
+**
+** QWASAR.IO -- my_add
+**
+** @param {int} param_1
+** @param {int} param_2
+**
+** @return {int}
+**
+*/
+
+int my_add(int nbr1, int nbr2)
+{
+    return(nbr1 + nbr2);
+}
