10d316bfa09c867bb18c22067944689c1cbe0c37
Bakare Muideen Adeleke <bakaremuideenadeleke@yahoo.com>
2023-02-21 21:41:00 +0100

    Quest completed

---
+++ Quest02/06-my_mult.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/*
+**
+** QWASAR.IO -- my_mult
+**
+** @param {int} param_1
+** @param {int} param_2
+**
+** @return {int}
+**
+*/
+
+int my_mult(int param_1, int param_2)
+{
+    return (param_1 * param_2);
+}
