a97bf7d822e189d4c4741f546ae40fe3cc0adc11
Bakare Muideen Adeleke <bakaremuideenadeleke@yahoo.com>
2023-02-21 21:42:19 +0100

    Quest completed

---
+++ Quest02/07-my_string_formatting.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/*
+**
+** QWASAR.IO -- my_string_formatting
+**
+** @param {char*} param_1
+** @param {char*} param_2
+** @param {int} param_3
+**
+** @return {void}
+**
+*/
+
+void my_string_formatting(char* param_1, char* param_2, int param_3)
+{
+    printf( "Hello, my name is %s %s, I'm %d.\n", param_1,param_2,param_3);
+}
