af41a4ae142c57c3f2556a95fc163c7fdc785faf
Bakare Muideen Adeleke <bakaremuideenadeleke@yahoo.com>
2023-02-21 21:45:03 +0100

    Quest completed

---
+++ Quest02/09-my_upcase.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/*
+**
+** QWASAR.IO -- my_upcase
+**
+** @param {char*} param_1
+**
+** @return {char*}
+**
+*/
+char* my_upcase(char* param_1)
+{
+    for (int i = 0; param_1[i] != '\0'; i++) {
+        if(param_1[i] >= 'a' && param_1[i] <= 'z') {
+         param_1[i] = param_1[i] -32;
+        }
+    }
+    return param_1;
+}
